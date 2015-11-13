83fbcfbe10ac109df66823dde8a44ad2ff0472f3
Jakub Křižka <email@jakubkrizka.cz>
2015-11-13 23:52:42 +0100

    nevim co se děje

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-// require components
-var express = require('express');
-var bodyParser = require('body-parser');
 var request = require('request');
 var cheerio = require('cheerio');
 var moment = require('moment');
-
-// app define
-var app = express();
-
-// define port running
-var port = process.env.PORT || 3000;
-
-// body parser middleware
-app.use(bodyParser.urlencoded({extended: true}));
-
-// test route
-app.get('/', function (req, res) {
-    res.status(200).send('Hello world, this is GET / route of nodejs-slack-lunch. Running on NodeJS - awesome framework :)')
+var stdio = require('stdio');
+var ops = stdio.getopt({
+    'restaurant': {args: 1, mandatory: true, description: 'Restaurant to parse'},
+    'url': {args: 1, mandatory: true, description: 'Url to post'}
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(400).send(err.message);
-});
-
-
-// console notice
-app.listen(port, function () {
-    console.log('nodejs-slack-lunch listening on port ' + port);
-});
-
-// define first restaurant 
-app.post('/racek', function (req, res) {
-    request('http://www.restaurace-racek.cz/sluzby/', function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-
-            $('.wsw table tbody').filter(function () {
+var url = null;
+var parse = null;
+switch (ops.restaurant) {
+    case 'racek':
+        url = 'http://www.restaurace-racek.cz/sluzby/';
+        parse = function ($) {
+            var text = null;
+            $('.wsw table tbody').each(function () {
                 var $this = $(this);
                 $this.html($this.html().replace(/\t|\n|[ ]{2}/g, ''));
                 $this.find("tr").each(function () {
@@ -56,23 +33,19 @@ app.post('/racek', function (req, res) {
                     }
                 });
 
-                return res.status(200).json({
-                    text: $this.text()
-                });
-
+                text = $this.text();
             })
-        }
-    })
-});
 
-app.post('/cestaci', function (req, res) {
-    request('http://www.hedvabnastezka.cz/klub-cestovatelu-brno/poledni-menu-2/', function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
+            return text;
+        };
+        break;
 
+    case 'cestovatele':
+        url = 'http://www.hedvabnastezka.cz/klub-cestovatelu-brno/poledni-menu-2/';
+        parse = function ($) {
             var date = moment().day();
-
-            $('.article-content p:nth-child(' + (2 + date) + ')').filter(function () {
+            var text = null;
+            $('.article-content p:nth-child(' + (2 + date) + ')').each(function () {
                 var $this = $(this);
                 $this.html($this.html().replace(/\t|\n|[ ]{2}/g, ''));
                 $this.find('strong').each(function () {
@@ -82,57 +55,22 @@ app.post('/cestaci', function (req, res) {
                 html = $this.html();
                 html = html.replace(/<br>/g, '\n');
 
-                var text = $(html).text().trim();
+                text = $(html).text().trim();
 
                 if (!text) {
                     text = 'nic';
                 }
+            });
 
-                return res.status(200).json({
-                    text: text
-                });
+            return text;
+        };
+        break;
 
-            })
-        }
-    })
-})
-
-app.post('/vietmac', function (req, res) {
-    request('https://www.zomato.com/cs/brno/pad-thai-kr%C3%A1lovo-pole-brno-sever/', function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-
-            $('#tabtop .menu-preview.bb5').filter(function () {
-                var $this = $(this);
-                $this.html($this.html().replace(/\t|\n|[ ]{2}/g, ''));
-
-                $this.find('.res-info-headline span.hdn').remove();
-                var span = $this.find('.res-info-headline span');
-                span.text('\t' + span.text());
-                var time = $this.find('.res-info-headline .dm-serving-time');
-                time.text('\t' + time.text() + '\n');
-                $this.find('.tmi-groups .tmi-group .tmi-daily .tmi-price').each(function(){
-                    var $this = $(this);
-                    $this.text('\t' + $this.text() + '\n');
-                });
-
-                return res.status(200).json({
-                    text: $this.text()
-                });
-
-            })
-        }
-    })
-});
-
-
-
-app.post('/ocean', function (req, res) {
-    request('http://www.ocean48.cz/bistro/nabidka', function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-
-            $('.content .left-half.col').filter(function () {
+    case 'ocean':
+        url = 'http://www.ocean48.cz/bistro/nabidka';
+        parse = function ($) {
+            var text = null;
+            $('.content .left-half.col').each(function () {
                 var $this = $(this);
                 $this.html($this.html().replace(/\r\n|\t|\n|[ ]{2}/g, ''));
 
@@ -143,23 +81,65 @@ app.post('/ocean', function (req, res) {
                 title.not(title = title.first()).remove();
                 title.text(title.text() + '\n');
 
-                menu.find('tr td.price').each(function(){
+                menu.find('tr td.price').each(function () {
                     var $this = $(this);
                     $this.text('\t' + $this.text() + '\n');
                 });
 
-                menu.find('tr td.menu-item-text-col span.small').each(function(){
+                menu.find('tr td.menu-item-text-col span.small').each(function () {
                     var $this = $(this);
-                    if ($this.text().trim()){
+                    if ($this.text().trim()) {
                         $this.text('\n\t' + $this.text());
                     }
                 });
 
-                return res.status(200).json({
-                    text: $this.text()
+                text = $this.text()
+
+            });
+            return text;
+        };
+        break;
+
+    case 'padthai':
+        url = 'https://www.zomato.com/cs/brno/pad-thai-kr%C3%A1lovo-pole-brno-sever/';
+        parse = function ($) {
+            var text = null;
+            $('#tabtop .menu-preview.bb5').filter(function () {
+                var $this = $(this);
+                $this.html($this.html().replace(/\t|\n|[ ]{2}/g, ''));
+
+                $this.find('.res-info-headline span.hdn').remove();
+                var span = $this.find('.res-info-headline span');
+                span.text('\t' + span.text());
+                var time = $this.find('.res-info-headline .dm-serving-time');
+                time.text('\t' + time.text() + '\n');
+                $this.find('.tmi-groups .tmi-group .tmi-daily .tmi-price').each(function () {
+                    var $this = $(this);
+                    $this.text('\t' + $this.text() + '\n');
                 });
 
+                text = $this.text();
+
+            });
+
+            return text;
+        };
+        break;
+
+    default:
+        console.log('neznámá restaurace');
+        process.exit();
+        break;
+
+}
+
+request(url, function (error, response, html) {
+    if (!error) {
+        request.post({
+            uri: ops.url,
+            form: JSON.stringify({
+                text: parse(cheerio.load(html))
             })
-        }
-    })
+        });
+    }
 });
